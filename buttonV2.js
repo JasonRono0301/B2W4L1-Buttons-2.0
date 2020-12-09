629e36a07c8dca04478d14f7ee2c3932bffc98f0
Jason R <99057964@mydavinci.nl>
2020-12-09 11:36:31 +0100

    Button van kleur veranderen.
    Heb als er op een button word gedrukt dat de anderen een andere kleur krijgen moet alleen nog de counters disabelen.

--- buttonV2.js
+++ buttonV2.js
@@ -9,7 +9,7 @@ var button3 = document.getElementById("button3");
 //Variabelen voor de images die in de html body staan.
 var container = document.getElementById('container');
 
-//Variabelen voor de counters voor hoeveel keer op de knop word gedrukt.
+//Variablen voor de counters.
 var count1 = 0;
 var count2 = 0;
 var count3 = 0;
@@ -17,16 +17,30 @@ var count3 = 0;
 button1.addEventListener('click',function(){
     button1.innerHTML = count1++;//Elke keer als er op 1 van de buttons worden gedrukt gaat er steeds 1 bij komen.
     button1.style.backgroundColor = "red";
+
+    if(button1.style.backgroundColor == "red"){
+        button2.style.backgroundColor = "green";
+        button3.style.backgroundColor = "green";
+    } 
 });
 
 button2.addEventListener('click',function(){
-    button2.innerHTML = count2++;
-    button2.style.backgroundColor = "green";
+    button2.innerHTML = count2++;//Zie regel 20!.
+    button2.style.backgroundColor = "red";
+
+    if(button2.style.backgroundColor == "red"){
+        button1.style.backgroundColor = "green";
+        button3.style.backgroundColor = "green";
+    } 
 
 });
 
 button3.addEventListener('click',function(){
-    button3.innerHTML = count3++;
-    button3.style.backgroundColor = "green";
+    button3.innerHTML = count3++;//Zie regel 20!.
+    button3.style.backgroundColor = "red";
 
+    if(button3.style.backgroundColor == "red"){
+        button1.style.backgroundColor = "green";
+        button2.style.backgroundColor = "green";
+    } 
 });
